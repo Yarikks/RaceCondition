5559a1a32ab23c9de01b01abe8ceab776bda242a
Yorik <jasper18poil@gmail.com>
2020-03-28 15:05:04 +0200

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace RaceCondition
+{
+    class Program
+    {
+        static int result = 0;
+
+        static void ChangeToOne() => result = 1;
+        static void ChangeToTwo() => result = 2;
+        static void ChangeToThree() => result = 3;
+
+        static void Main(string[] args)
+        {
+            for(int i = 0; i < 20; i++)
+            {
+                Thread firstThread = new Thread(ChangeToOne);
+                Thread secondThread = new Thread(ChangeToTwo);
+                Thread thirdThread = new Thread(ChangeToThree);
+
+                firstThread.Start();
+                secondThread.Start();
+                thirdThread.Start();
+
+                Console.WriteLine($"Result is {result}");
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
